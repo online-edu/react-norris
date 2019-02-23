78e3a3557157f65c4adfb2845f02973b89d8af75
Hardik Pithva <hardikpthv@gmail.com>
2019-02-23 21:01:59 +0530

    feat(view): add jokes component

---
+++ src/pages/jokes/Jokes.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react';
+import { List } from '../../components';
+import { loadJokes, onFavorite, loadFavoriteJokes } from './JokesService';
+/**
+ * Jokes component.
+ */
+class Jokes extends Component {
+  /**
+   * Intilalizes the state and binds all methods.
+   *
+   * @param {Object} props - access 'props'
+   */
+  constructor(props) {
+    super(props);
+    this.state = {
+      jokes: [],
+      favoriteJokes: [],
+      loading: true,
+      error: false,
+      errMsg: '',
+    };
+    this.loadJokes = this.loadJokes.bind(this);
+    this.onFavoriteClick = this.onFavoriteClick.bind(this);
+  }
+
+  /**
+   * Call `loadJokes()` and fetch fa once component is mounted.
+   */
+  componentDidMount() {
+    this.loadJokes();
+    this.setState({ favoriteJokes: loadFavoriteJokes() });
+  }
+
+  /**
+   * On favortite click of particular joke
+   *
+   * @param {Object} joke - joke
+   * @param {boolean} fav - fav or un-fav
+   */
+  onFavoriteClick({ item: joke, fav }) {
+    const jokes = onFavorite(joke, fav);
+    this.setState({ favoriteJokes: jokes });
+  }
+
+  /**
+   * Load jokes once component is mounted.
+   */
+  loadJokes() {
+    loadJokes()
+      .then(jokes => this.setState({ jokes, loading: false }))
+      .catch(err => this.handleError(err));
+  }
+
+  /**
+   * Handles common errors for n/w requests.
+   *
+   * @param {string} err - Error message
+   */
+  handleError(err) {
+    this.setState({ loading: false, error: true, errMsg: err.toString() });
+  }
+
+  /**
+   * Render method for component
+   */
+  render() {
+    const { jokes, favoriteJokes, loading } = this.state;
+
+    return (
+      <div className="container-fluid px-4 pt-4">
+        <div className="row">
+          <div className="col-md-6 col-sm-12 mb-4">
+            <div className="card">
+              <List
+                title={`Jokes (${jokes.length})`}
+                btnCaption="View more"
+                loader={loading}
+                items={jokes}
+                onHeaderAction={this.loadJokes}
+                onFavoriteClick={this.onFavoriteClick}
+              />
+            </div>
+          </div>
+          <div className="col-md-6 col-sm-12 mb-4">
+            <div className="card">
+              <List
+                favorite
+                title="Favorite Jokes"
+                loader={loading}
+                items={favoriteJokes}
+                onFavoriteClick={this.onFavoriteClick}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Jokes;
