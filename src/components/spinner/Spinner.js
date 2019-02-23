c0876bdc2128e514ebb4f5508f6688166122ae8c
Hardik Pithva <hardikpthv@gmail.com>
2019-02-23 18:23:44 +0530

    feat(component): add spinner

---
+++ src/components/spinner/Spinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+/**
+ * Spinner component.
+ */
+const Spinner = () => (
+  <div className="spinner-border text-primary my-2" role="status">
+    <span className="sr-only">Loading...</span>
+  </div>
+);
+
+export default Spinner;
