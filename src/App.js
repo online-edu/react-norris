ad319bd7824448d47486c2b81b69c1fc8ea40175
Hardik Pithva <hardikpthv@gmail.com>
2019-02-23 21:27:51 +0530

    chore(component): update with header and footer component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
-
+import React from 'react';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import { Header, Footer } from './components';
 import Jokes from './pages/jokes/Jokes';
+
 /**
  * App component.
  */
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <Switch>
-          <Redirect from="/" exact to="/jokes" />
-          <Route path="/jokes" component={Jokes} />
-        </Switch>
-      </BrowserRouter>
-    );
-  }
-}
+const App = () => (
+  <div className="jokes-container">
+    <Header />
+    <BrowserRouter>
+      <Switch>
+        <Redirect from="/" exact to="/jokes" />
+        <Route path="/jokes" component={Jokes} />
+      </Switch>
+    </BrowserRouter>
+    <Footer />
+  </div>
+);
 
 export default App;
