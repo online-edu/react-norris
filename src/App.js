52d4fb2778c945fc49ec5ccf3e0a89a8b6da4c6d
Hardik Pithva <hardikpthv@gmail.com>
2019-02-23 17:11:40 +0530

    feat: add react support and basic template

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const myApp = 'Welcome to Chuck Norris Jokes!';
+
+class App extends Component {
+  render() {
+    return <div>{myApp}</div>;
+  }
+}
+
+export default App;
