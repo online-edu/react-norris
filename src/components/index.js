7583f0e370506cd9eada07342250833d01a12c89
Hardik Pithva <hardikpthv@gmail.com>
2019-02-24 01:15:20 +0530

    feat(component): add switch

--- src/components/index.js
+++ src/components/index.js
@@ -3,9 +3,11 @@ import Footer from './footer';
 import Header from './header';
 import List from './list';
 import Spinner from './spinner';
+import Switch from './switch';
 
 export {
   Spinner,
+  Switch,
   List,
   FavButton,
   Header,
