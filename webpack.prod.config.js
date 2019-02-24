b23d06e06154ad6f78ed597b9708187b405b2f5d
Hardik Pithva <hardikpthv@gmail.com>
2019-02-24 20:08:20 +0530

    build: add webpack prod config

---
+++ webpack.prod.config.js
@@ -0,0 +1,58 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const htmlPlugin = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: './index.html',
+  favicon: 'src/favicon.png',
+});
+
+const cssPlugin = new MiniCssExtractPlugin({
+  filename: '[name].[hash].css',
+  chunkFilename: '[id].[hash].css',
+});
+
+module.exports = {
+  entry: ['@babel/polyfill', './src'],
+  output: {
+    chunkFilename: '[name].[hash].js',
+    filename: '[name].[hash].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: false,
+        uglifyOptions: {
+          output: {
+            comments: false,
+          },
+        },
+      }),
+      new OptimizeCSSAssetsPlugin({}),
+    ],
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+  plugins: [htmlPlugin, cssPlugin, new CleanWebpackPlugin(['dist/*'])],
+};
